5940f88fb5a6263b325271baa52406cf86913cd5
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-07 16:13:54 +0700

    chore(config): setup dev server port setted by env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
+    devtools: {
+        enabled: true,
+    },
+
     typescript: {
         typeCheck: true,
         strict: true,
+    },
+
+    devServer: {
+        port: Number(process.env.NUXT_DEV_PORT) ?? 3000,
     },
 });
