c0e5cfd67c3f49d9fda9160ac04a6f30be3ef5cf
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-07 16:36:34 +0700

    chore(config): setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import TailwindCSS from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
+
     devtools: {
         enabled: true,
     },
@@ -13,4 +16,16 @@ export default defineNuxtConfig({
     devServer: {
         port: Number(process.env.NUXT_DEV_PORT) ?? 3000,
     },
+
+    css: [
+        './app/styles/index.css',
+    ],
+
+    vite: {
+        plugins: [
+            // https://github.com/tailwindlabs/tailwindcss
+            TailwindCSS(),
+        ],
+    },
+
 });
